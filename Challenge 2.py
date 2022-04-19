a643a4e4426b21d48cac9c86c5c9e2734cbc0bf2
01023749189o <97387353+01023749189o@users.noreply.github.com>
2022-04-19 02:35:25 +0200

    Add files via upload

---
+++ Challenge 2.py
@@ -0,0 +1,20 @@
+guess = input("What's my favorite food? ")
+
+if guess == "cookies":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That’s not it!")
+
+print("Thanks for playing!")
+
+# output
+# 
+# guess == cookies
+#
+# Yep! So amazing!
+# Thanks for playing!
+# 
+# guess != cookies
+#
+# Yuck! That’s not it!
+# Thanks for playing!
---
+++ ‏‏Challenge 3.py
@@ -0,0 +1,11 @@
+# Prints out the name of a favorite city
+def favorite_city(name):
+    print("One of my favorite cities is", name)
+
+favorite_city("shirbin, Dakahlia")
+favorite_city("Asheville, North Carolina")
+favorite_city("Amsterdam, The Netherlands")
+# output
+# One of my favorite cities is shirbin, Dakahlia
+# One of my favorite cities is Asheville, North Carolina
+# One of my favorite cities is Amsterdam, The Netherlands
